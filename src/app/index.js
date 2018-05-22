e8386795a156687b72c625be50003385828a1f11
Taddes Korris <taddes.korris@gmail.com>
2018-05-21 21:24:24 -0400

    passing data into an ul with jsx

--- src/app/index.js
+++ src/app/index.js
@@ -4,16 +4,31 @@ var createReactClass = require('create-react-class');
 
 // Create Component
 var TodoComponent = createReactClass({
+  getInitialState: function() {
+    return {
+      todos: ['brush teeth', 'exercise for 20 min', 'play the cat', 'eat breakfast']
+    }
+  },
   render: function() {
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return(
+        <li>{item}</li>
+      )
+    });
     return (
       <div>
-      <h1>Hello!</h1>
-      <p>This is a test</p>
-      <p>{this.props.mssg}</p>
-      <hr/>
-      <p><strong>My Cat: </strong>{this.props.pepper.name}</p>
-      <p><strong>My Cat's Breed: </strong>{this.props.pepper.breed}</p>
-      <p><strong>My Cat's Attributes: </strong>{this.props.pepper.attributes}</p>
+        <h1>Hello!</h1>
+        <p>This is a test</p>
+        <p>{this.props.mssg}</p>
+        <hr/>
+        <p><strong>My Cat: </strong>{this.props.pepper.name}</p>
+        <p><strong>My Cat's Breed: </strong>{this.props.pepper.breed}</p>
+        <p><strong>My Cat's Attributes: </strong>{this.props.pepper.attributes}</p>
+
+      <div id="todo-list">
+        <ul>{todos}</ul>
+      </div>
       
       </div>
     );
