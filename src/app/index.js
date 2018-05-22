0c022763c2d9e0ef72fd8c7aa70c2b578939c1f0
Taddes Korris <taddes.korris@gmail.com>
2018-05-21 21:07:18 -0400

    included createReactClass data, new component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-alert('It work');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+// Create Component
+var TodoComponent = createReactClass({
+  render: function() {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
+});
+
+// Put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'))
